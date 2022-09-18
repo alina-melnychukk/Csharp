67a8325cb167e52a5d62029b58646e8c238bef19
alina-melnychukk <alinameln2@gmail.com>
2022-09-18 20:03:27 +0300

    Lab 2

---
+++ Lab2/Lab2/Lab2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn = 0;
+            int nk = 0;
+            double result = 0;
+            bool isNNOk = false;
+            bool isNKOk = false;
+
+            do
+            {
+                Console.Write("Введіть значення nn: ");
+                string strNN = Console.ReadLine();
+                isNNOk = int.TryParse(strNN, out nn);
+                if (isNNOk && nn >= 0) break;
+                    Console.WriteLine("Помилка введення");
+            } while (!isNNOk);
+
+            do
+            {
+                Console.Write("Введіть значення nk: ");
+                string strNK = Console.ReadLine();
+                isNKOk = int.TryParse(strNK, out nk);
+                if (isNKOk) isNKOk = nk >= nn;
+                if (isNKOk) break;
+                Console.WriteLine("Помилка введення");
+            } while(!isNKOk);
+
+            for (int k = nn; k < nk; k++)
+            {
+                result += (Math.Pow(k, 2) - 3) / (Math.Pow(k, 2) - Math.Pow(-1, k) * k + 3); 
+            }
+
+           
+            Console.Write("Результат=");
+            Console.WriteLine(result);
+        }
+    }
+}
